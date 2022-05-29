a97c5dffc25074dcd8a0c95639247a8516c89953
Thomas Patzke <thomas@patzke.org>
2022-05-29 10:54:38 +0200

    Improved backend tokens: examples and documentation

--- {{ cookiecutter.package_name }}/sigma/backends/{{ cookiecutter.backend_package_name }}/{{ cookiecutter.backend_package_name }}.py
+++ {{ cookiecutter.package_name }}/sigma/backends/{{ cookiecutter.backend_package_name }}/{{ cookiecutter.backend_package_name }}.py
@@ -8,29 +8,59 @@ from typing import ClassVar, Dict, List, Optional, Tuple
 
 class {{ cookiecutter.backend_class_name }}(TextQueryBackend):
     """{{ cookiecutter.target_name }} backend."""
-    # TODO: change the token definitions according to the syntax.
+    # TODO: change the token definitions according to the syntax. Delete these not supported by your backend.
     # See the pySigma documentation for further infromation:
     # https://sigmahq-pysigma.readthedocs.io/en/latest/Backends.html
-    group_expression : ClassVar[str] = "({expr})"
 
+    # Operator precedence: tuple of Condition{AND,OR,NOT} in order of precedence.
+    # The backend generates grouping if required
+    precedence : ClassVar[Tuple[ConditionItem, ConditionItem, ConditionItem]] = (ConditionNOT, ConditionAND, ConditionOR)
+    group_expression : ClassVar[str] = "({expr})"   # Expression for precedence override grouping as format string with {expr} placeholder
+
+    # Generated query tokens
+    token_separator : str = " "     # separator inserted between all boolean operators
     or_token : ClassVar[str] = "OR"
     and_token : ClassVar[str] = " "
     not_token : ClassVar[str] = "NOT"
-    eq_token : ClassVar[str] = "="
+    eq_token : ClassVar[str] = "="  # Token inserted between field and value (without separator)
 
-    str_quote : ClassVar[str] = '"'
-    escape_char : ClassVar[str] = "\\"
-    wildcard_multi : ClassVar[str] = "*"
-    wildcard_single : ClassVar[str] = "*"
-    add_escaped : ClassVar[str] = "\\"
+    # String output
+    ## Fields
+    field_quote : ClassVar[str] = "'"                               # Character used to quote field characters if field_quote_pattern matches (or not, depending on field_quote_pattern_negation). No field name quoting is done if not set.
+    field_quote_pattern : ClassVar[Pattern] = re.compile("^\w+$")   # Quote field names if this pattern (doesn't) matches, depending on field_quote_pattern_negation. Field name is always quoted if pattern is not set.
+    field_quote_pattern_negation : ClassVar[bool] = True            # Negate field_quote_pattern result. Field name is quoted if pattern doesn't matches if set to True (default).
 
-    re_expression : ClassVar[str] = "{regex}"
-    re_escape_char : ClassVar[str] = "\\"
-    re_escape : ClassVar[Tuple[str]] = ('"',)
+    ## Values
+    str_quote       : ClassVar[str] = '"'     # string quoting character (added as escaping character)
+    escape_char     : ClassVar[str] = "\\"    # Escaping character for special characrers inside string
+    wildcard_multi  : ClassVar[str] = "*"     # Character used as multi-character wildcard
+    wildcard_single : ClassVar[str] = "*"     # Character used as single-character wildcard
+    add_escaped     : ClassVar[str] = "\\"    # Characters quoted in addition to wildcards and string quote
+    filter_chars    : ClassVar[str] = ""      # Characters filtered
+    bool_values     : ClassVar[Dict[bool, str]] = {   # Values to which boolean values are mapped.
+        True: "true",
+        False: "false",
+    }
 
-    cidr_expression : ClassVar[str] = "{value}"
+    # String matching operators. if none is appropriate eq_token is used.
+    startswith_expression : ClassVar[str] = "startswith"
+    endswith_expression   : ClassVar[str] = "endswith"
+    contains_expression   : ClassVar[str] = "contains"
+    wildcard_match_expression : ClassVar[str] = "match"      # Special expression if wildcards can't be matched with the eq_token operator
 
-    compare_op_expression : ClassVar[str] = "{field}{operator}{value}"
+    # Regular expressions
+    re_expression : ClassVar[str] = "{field}=~{value}"  # Regular expression query as format string with placeholders {field} and {regex}
+    re_escape_char : ClassVar[str] = "\\"               # Character used for escaping in regular expressions
+    re_escape : ClassVar[Tuple[str]] = ()               # List of strings that are escaped
+
+    # cidr expressions
+    cidr_wildcard : ClassVar[str] = "*"    # Character used as single wildcard
+    cidr_expression : ClassVar[Optional[str]] = "cidrmatch({field}, {value})"    # CIDR expression query as format string with placeholders {field} = {value}
+    cidr_in_list_expression : ClassVar[Optional[str]] = "{field} in ({value})"    # CIDR expression query as format string with placeholders {field} = in({list})
+
+    # Numeric comparison operators
+    compare_op_expression : ClassVar[str] = "{field}{operator}{value}"  # Compare operation query as format string with placeholders {field}, {operator} and {value}
+    # Mapping between CompareOperators elements and strings used as replacement for {operator} in compare_op_expression
     compare_operators : ClassVar[Dict[SigmaCompareExpression.CompareOperators, str]] = {
         SigmaCompareExpression.CompareOperators.LT  : "<",
         SigmaCompareExpression.CompareOperators.LTE : "<=",
@@ -38,22 +68,27 @@ class {{ cookiecutter.backend_class_name }}(TextQueryBackend):
         SigmaCompareExpression.CompareOperators.GTE : ">=",
     }
 
-    field_null_expression : ClassVar[str] = "{field}!=*"
+    # Null/None expressions
+    field_null_expression : ClassVar[str] = "{field} is null"          # Expression for field has null value as format string with {field} placeholder for field name
 
-    convert_or_as_in : ClassVar[bool] = True
-    convert_and_as_in : ClassVar[bool] = False
-    in_expressions_allow_wildcards : ClassVar[bool] = True
-    field_in_list_expression : ClassVar[str] = "{field} {op} ({list})"
-    or_in_operator : ClassVar[Optional[str]] = "IN"
-    list_separator : ClassVar[str] = ", "
+    # Field value in list, e.g. "field in (value list)" or "field containsall (value list)"
+    convert_or_as_in : ClassVar[bool] = True                     # Convert OR as in-expression
+    convert_and_as_in : ClassVar[bool] = True                    # Convert AND as in-expression
+    in_expressions_allow_wildcards : ClassVar[bool] = True       # Values in list can contain wildcards. If set to False (default) only plain values are converted into in-expressions.
+    field_in_list_expression : ClassVar[str] = "{field} {op} ({list})"  # Expression for field in list of values as format string with placeholders {field}, {op} and {list}
+    or_in_operator : ClassVar[str] = "in"               # Operator used to convert OR into in-expressions. Must be set if convert_or_as_in is set
+    and_in_operator : ClassVar[str] = "contains-all"    # Operator used to convert AND into in-expressions. Must be set if convert_and_as_in is set
+    list_separator : ClassVar[str] = ", "               # List element separator
 
-    unbound_value_str_expression : ClassVar[str] = '"{value}"'
-    unbound_value_num_expression : ClassVar[str] = '{value}'
-    unbound_value_re_expression : ClassVar[str] = '{value}'
+    # Value not bound to a field
+    unbound_value_str_expression : ClassVar[str] = '"{value}"'   # Expression for string value not bound to a field as format string with placeholder {value}
+    unbound_value_num_expression : ClassVar[str] = '{value}'   # Expression for number value not bound to a field as format string with placeholder {value}
+    unbound_value_re_expression : ClassVar[str] = '_=~{value}'    # Expression for regular expression not bound to a field as format string with placeholder {value}
 
-    deferred_start : ClassVar[str] = "\n| "
-    deferred_separator : ClassVar[str] = "\n| "
-    deferred_only_query : ClassVar[str] = "*"
+    # Query finalization: appending and concatenating deferred query part
+    deferred_start : ClassVar[str] = "\n| "               # String used as separator between main query and deferred parts
+    deferred_separator : ClassVar[str] = "\n| "           # String used to join multiple deferred query parts
+    deferred_only_query : ClassVar[str] = "*"            # String used as query if final query only contains deferred expression
 
     # TODO: implement custom classes for query elements not covered by the default backend base.
     # Documentation: https://sigmahq-pysigma.readthedocs.io/en/latest/Backends.html
