fc120bd7710aea4da48c192acceca8ade951a17f
Thomas Patzke <thomas@patzke.org>
2022-07-05 00:54:27 +0200

    Added explicit coverage development dependency

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,3 +3,4 @@ import subprocess
 subprocess.run(["poetry", "add", "pysigma"])
 subprocess.run(["poetry", "add", "-D", "pytest"])
 subprocess.run(["poetry", "add", "-D", "pytest-cov"])
+subprocess.run(["poetry", "add", "-D", "coverage"])
