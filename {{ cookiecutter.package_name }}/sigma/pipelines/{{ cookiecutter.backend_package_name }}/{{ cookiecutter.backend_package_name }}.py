e82d9a6d7d466fe61421a0a24451a749d2d45b60
Thomas Patzke <thomas@patzke.org>
2022-08-03 02:36:50 +0200

    Using windows logsource mapping from pySigma

--- {{ cookiecutter.package_name }}/sigma/pipelines/{{ cookiecutter.backend_package_name }}/{{ cookiecutter.backend_package_name }}.py
+++ {{ cookiecutter.package_name }}/sigma/pipelines/{{ cookiecutter.backend_package_name }}/{{ cookiecutter.backend_package_name }}.py
@@ -1,17 +1,11 @@
-from sigma.pipelines.common import logsource_windows
+from sigma.pipelines.common import logsource_windows, windows_logsource_mapping
 from sigma.processing.transformations import AddConditionTransformation, FieldMappingTransformation, DetectionItemFailureTransformation, RuleFailureTransformation, SetStateTransformation
 from sigma.processing.conditions import LogsourceCondition, IncludeFieldCondition, ExcludeFieldCondition, RuleProcessingItemAppliedCondition
 from sigma.processing.pipeline import ProcessingItem, ProcessingPipeline
 
+
 # TODO: the following code is just an example extend/adapt as required.
 # See https://sigmahq-pysigma.readthedocs.io/en/latest/Processing_Pipelines.html for further documentation.
-
-windows_logsource_mapping = {       # define mappings and iterate over them in pipeline definitions to get more readable pipeline definitions.
-    "security": "WinEventLog:Security",
-    "application": "WinEventLog:Application",
-    "system": "WinEventLog:System",
-    "sysmon": "WinEventLog:Microsoft-Windows-Sysmon/Operational",
-}
 
 def {{ cookiecutter.backend_package_name }}_example():      # Processing pipelines should be defined as functions that return a ProcessingPipeline object.
     return ProcessingPipeline(
