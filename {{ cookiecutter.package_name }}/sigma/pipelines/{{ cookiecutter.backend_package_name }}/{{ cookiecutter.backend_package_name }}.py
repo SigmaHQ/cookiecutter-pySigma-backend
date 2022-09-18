a9a51c6db1f06a14d95a0c9ddc3d657726137766
Thomas Patzke <thomas@patzke.org>
2022-09-18 10:59:31 +0200

    Pipeline typing

--- {{ cookiecutter.package_name }}/sigma/pipelines/{{ cookiecutter.backend_package_name }}/{{ cookiecutter.backend_package_name }}.py
+++ {{ cookiecutter.package_name }}/sigma/pipelines/{{ cookiecutter.backend_package_name }}/{{ cookiecutter.backend_package_name }}.py
@@ -3,11 +3,10 @@ from sigma.processing.transformations import AddConditionTransformation, FieldMa
 from sigma.processing.conditions import LogsourceCondition, IncludeFieldCondition, ExcludeFieldCondition, RuleProcessingItemAppliedCondition
 from sigma.processing.pipeline import ProcessingItem, ProcessingPipeline
 
-
 # TODO: the following code is just an example extend/adapt as required.
 # See https://sigmahq-pysigma.readthedocs.io/en/latest/Processing_Pipelines.html for further documentation.
 
-def {{ cookiecutter.backend_package_name }}_example():      # Processing pipelines should be defined as functions that return a ProcessingPipeline object.
+def {{ cookiecutter.backend_package_name }}_example() -> ProcessingPipeline:      # Processing pipelines should be defined as functions that return a ProcessingPipeline object.
     return ProcessingPipeline(
         name="{{ cookiecutter.target_name }} example pipeline",
         priority=20,            # The priority defines the order pipelines are applied. See documentation for common values.
