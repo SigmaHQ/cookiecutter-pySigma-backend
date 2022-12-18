1eeadd158d25be5ccbe3eeee3e07784d019630cf
Thomas Patzke <thomas@patzke.org>
2022-12-19 00:08:20 +0100

    Added allowed_backends to processing pipeline template

--- {{ cookiecutter.package_name }}/sigma/pipelines/{{ cookiecutter.backend_package_name }}/{{ cookiecutter.backend_package_name }}.py
+++ {{ cookiecutter.package_name }}/sigma/pipelines/{{ cookiecutter.backend_package_name }}/{{ cookiecutter.backend_package_name }}.py
@@ -6,9 +6,10 @@ from sigma.processing.pipeline import ProcessingItem, ProcessingPipeline
 # TODO: the following code is just an example extend/adapt as required.
 # See https://sigmahq-pysigma.readthedocs.io/en/latest/Processing_Pipelines.html for further documentation.
 
-def {{ cookiecutter.backend_package_name }}_example() -> ProcessingPipeline:      # Processing pipelines should be defined as functions that return a ProcessingPipeline object.
+def {{ cookiecutter.backend_package_name }}_example() -> ProcessingPipeline:        # Processing pipelines should be defined as functions that return a ProcessingPipeline object.
     return ProcessingPipeline(
         name="{{ cookiecutter.target_name }} example pipeline",
+        allowed_backends=frozenset(),                                               # Set of identifiers of backends (from the backends mapping) that are allowed to use this processing pipeline. This can be used by frontends like Sigma CLI to warn the user about inappropriate usage.
         priority=20,            # The priority defines the order pipelines are applied. See documentation for common values.
         items=[
             ProcessingItem(     # This is an example for processing items generated from the mapping above.
