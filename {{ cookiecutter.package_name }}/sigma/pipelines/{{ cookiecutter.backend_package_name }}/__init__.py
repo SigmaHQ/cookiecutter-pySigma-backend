580493fab30e29afe4567567f2b58c9925885ab4
Thomas Patzke <thomas@patzke.org>
2024-02-10 01:36:43 +0100

    Remove legacy directorry dict for backends and pipelines

--- {{ cookiecutter.package_name }}/sigma/pipelines/{{ cookiecutter.backend_package_name }}/__init__.py
+++ {{ cookiecutter.package_name }}/sigma/pipelines/{{ cookiecutter.backend_package_name }}/__init__.py
@@ -1,6 +1,2 @@
 from .{{ cookiecutter.backend_package_name }} import {{ cookiecutter.backend_package_name }}_pipeline
 # TODO: add all pipelines that should be exposed to the user of your backend in the import statement above.
-
-pipelines = {
-    "{{ cookiecutter.backend_package_name }}_pipeline": {{ cookiecutter.backend_package_name }}_pipeline,   # TODO: adapt identifier to something approproiate
-}
